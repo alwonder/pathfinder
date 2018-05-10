b1d760d751b895d384000cf92a2cbfd0e3bec5f5
Sergey Eliseev <thefury014@gmail.com>
2018-05-10 17:21:41 +0900

    BFS path find with stopping on destination reach

--- app.js
+++ app.js
@@ -159,8 +159,52 @@ function getPathToSource(directionsMap, destination) {
     return path;
 }
 
-const before = Date.now();
 
+function findPathBFS(grid, startPoint, destination) {
+    const frontier = [];
+    let count = 0;
+    frontier.push(startPoint);
+
+    const directionsMap = new Map();
+    directionsMap.set(startPoint, null);
+    let foundNode = false;
+
+    while (frontier.length > 0 && !foundNode) {
+        const current = frontier.shift();
+        /* eslint no-loop-func: 0 */
+        grid.getNeighbours(current).forEach((neighbour) => {
+            if (!directionsMap.has(neighbour)) {
+                count++;
+                frontier.push(neighbour);
+                directionsMap.set(neighbour, current);
+                if (neighbour.x === destination.x && neighbour.y === destination.y) {
+                    foundNode = true;
+                }
+            }
+        });
+    }
+    console.log(`Iterations: ${count}`);
+
+    let destinationPoint;
+    directionsMap.forEach((cameFrom, node) => {
+        if (node.x === destination.x && node.y === destination.y) {
+            destinationPoint = node;
+        }
+    });
+    if (destinationPoint === undefined) return null;
+
+    const path = [];
+    path.push(destinationPoint);
+
+    let nbr = directionsMap.get(destinationPoint);
+    while (directionsMap.get(nbr) !== null) {
+        path.push(nbr);
+        nbr = directionsMap.get(nbr);
+    }
+    return path;
+}
+
+const createTime = Date.now();
 const grid = new MapGrid({
     x: 0, y: 0, w: 100, h: 100,
 }, [
@@ -179,12 +223,13 @@ const grid = new MapGrid({
     { x: 6, y: 4 },
 ]);
 
-console.log((Date.now() - before) / 1000);
+console.log(`Time for grid create: ${(Date.now() - createTime) / 1000}`);
 
-passThroughGraph(grid, { x: 0, y: 0});
+// const cameFromMap = passWithSource(grid, { x: 0, y: 0 });
+// const path2 = getPathToSource(cameFromMap, { x: 54, y: 35 });
 
-const cameFromMap = passWithSource(grid, { x: 0, y: 0});
-console.log((Date.now() - before) / 1000);
-
-const path = getPathToSource(cameFromMap, {x: 98, y: 96});
-console.log((Date.now() - before) / 1000);
+// passThroughGraph(grid, { x: 0, y: 0});
+const pathfindTime = Date.now();
+const path = findPathBFS(grid, { x: 0, y: 0 }, { x: 4, y: 5 });
+console.log(`Time to find a path: ${(Date.now() - pathfindTime) / 1000}`);
+// debugger
