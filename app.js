3ad11db498fd850450f16f4c675c3a3b370a609f
Sergey Eliseev <thefury014@gmail.com>
2018-05-17 15:29:59 +0900

    Grid initialization with connectNodes parameter

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const MapGrid = require('./MapGrid');
 const findPathBFS = require('./BFS');
 
 const createTime = Date.now();
-const grid = new MapGrid(input, input.borders);
+const grid = new MapGrid(input, true, input.borders);
 console.log(`Time for grid create: ${(Date.now() - createTime) / 1000}`);
 
 const pathfindTime = Date.now();
-findPathBFS(grid, { x: 7, y: 6 }, { x: 1, y: 1 }, true)
+findPathBFS(grid, { x: 7, y: 6 }, { x: 120, y: 120 }, false)
     .then((path) => {
         if (path === null) {
             console.log(`Couldn't find a path to the tile. Elapsed time: ${(Date.now() - pathfindTime) / 1000}`);
