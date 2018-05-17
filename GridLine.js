4e37ccfb7ad9c4e1fe3e62193acdb3a8c0317d2e
Sergey Eliseev <thefury014@gmail.com>
2018-05-17 09:31:51 +0900

    coloured output

--- GridLine.js
+++ GridLine.js
@@ -1,3 +1,14 @@
+const colorize = {
+    black(text) { return `\x1b[30m${text}\x1b[0m`; },
+    red(text) { return `\x1b[31m${text}\x1b[0m`; },
+    green(text) { return `\x1b[32m${text}\x1b[0m`; },
+    yellow(text) { return `\x1b[33m${text}\x1b[0m`; },
+    blue(text) { return `\x1b[34m${text}\x1b[0m`; },
+    magenta(text) { return `\x1b[35m${text}\x1b[0m`; },
+    cyan(text) { return `\x1b[36m${text}\x1b[0m`; },
+    white(text) { return `\x1b[37m${text}\x1b[0m`; },
+};
+
 class GridLine {
     constructor() {
         this.line = '';
@@ -18,10 +29,10 @@ class GridLine {
     static getTileSymbol(type) {
         switch (type) {
         case 'bound': return '  ';
-        case 'source': return '▪▪';
-        case 'destination': return 'ds';
-        case 'path': return '▢▢';
-        case 'search': return '▨▨';
+        case 'source': return colorize.red('██');
+        case 'destination': return colorize.blue('██');
+        case 'path': return colorize.green('██');
+        case 'search': return colorize.cyan('██');
         default: return '██';
         }
     }
